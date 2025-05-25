490a580eef2ab9772f2fc2ccd1290c426fcf1b82
LolghElmo <ghaithomo@hotmail.com>
2025-05-25 21:02:44 +0300

    Solved GcdOfStrings

---
+++ LeetCode75Solutions.ClassLibrary/ArrayStringProblems/GreatestCommonDivisorOfStrings.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Numerics;
+using System.Text;
+
+namespace LeetCode75Solutions.ClassLibrary.ArrayStringProblems
+{
+    public static class GreatestCommonDivisorOfStrings
+    {
+        public static string GcdOfStrings(string str1, string str2)
+        {
+            if (!(str1 + str2).Equals(str2 + str1))
+                return string.Empty;
+
+            int gcdLen = Gcd(str1.Length, str2.Length);
+            return str1.Substring(0, gcdLen);
+        }
+
+        private static int Gcd(int a, int b)
+        {
+            return b == 0 ? a : Gcd(b, a % b);
+        }
+
+
+        public static int DivideStrings(string dividend, string divisor)
+        {
+            if (string.IsNullOrWhiteSpace(dividend) || string.IsNullOrWhiteSpace(divisor))
+                throw new ArgumentException("Inputs must be non‐empty decimal strings.");
+
+            BigInteger num1 = BigInteger.Parse(dividend);
+            BigInteger num2 = BigInteger.Parse(divisor);
+            if (num2.IsZero)
+                throw new DivideByZeroException("Divisor cannot be zero.");
+
+            BigInteger quotient = num1 / num2;
+
+            if (quotient > int.MaxValue || quotient < int.MinValue)
+                throw new OverflowException("Result does not fit in an Int32.");
+
+            return (int)quotient;
+        }
+    }
+}
