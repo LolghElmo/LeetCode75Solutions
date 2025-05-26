dbb1a8753fcd76b79335da624d6177a29dc3eb7a
LolghElmo <ghaithomo@hotmail.com>
2025-05-27 00:37:45 +0300

    Added more problems

---
+++ LeetCode75Solutions.ClassLibrary/ArrayStringProblems/GreatestKidsWithCandies.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace LeetCode75Solutions.ClassLibrary.ArrayStringProblems
+{
+    public static class GreatestKidsWithCandies
+    {
+        public static IList<bool> KidsWithCandies(int[] candies, int extraCandies)
+        {
+            //// inefficient solution:
+            //    var boolResult = new List<bool>();
+
+            //    for (int i = 0; i < candies.Length; i++)
+            //    {
+            //        int currentCandies = candies[i] + extraCandies;
+            //        bool isGreatest = true;
+
+            //        for (int j = 0; j < candies.Length; j++)
+            //        {
+            //            if (currentCandies < candies[j])
+            //            {
+            //                isGreatest = false;
+            //                break;
+            //            }
+            //        }
+
+            //        boolResult.Add(isGreatest);
+            //    }
+
+            //    return boolResult;
+            //}
+
+            IList<bool> result = new List<bool>();
+            int maxCandies = candies.Max();
+
+            foreach (var candy in candies)
+            {
+                result.Add(candy + extraCandies >= maxCandies);
+            }
+
+            return result;
+        }
+    }
+}
---
+++ LeetCode75Solutions.ClassLibrary/ArrayStringProblems/PlaceFlowers.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace LeetCode75Solutions.ClassLibrary.ArrayStringProblems
+{
+    public class PlaceFlowers
+    {
+        public static bool CanPlaceFlowers(int[] flowerbed, int n)
+        {
+            for (int i = 0; i < flowerbed.Length; i++)
+            {
+                if (n == 0)
+                    return true;
+                if (flowerbed[i] == 0)
+                {
+                    bool leftIsEmpty = i == 0 || flowerbed[i - 1] == 0;
+                    bool rightIsEmpty = i == flowerbed.Length - 1 || flowerbed[i + 1] == 0;
+
+                    if (leftIsEmpty && rightIsEmpty)
+                    {
+                        n--;
+
+                        flowerbed[i] = 1;
+                    }
+                }
+            }
+            return n == 0;
+        }
+    }
+}
